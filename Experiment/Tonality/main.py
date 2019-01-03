70aada5e7ad90a26db9677587d34c9dc1059c2e2
@Arnold0210 <arnoldjulianherreraquiones@gmail.com>
2019-01-02 23:27:01 -0500

    C00001
    Sync

--- Experiment/Tonality/main.py
+++ Experiment/Tonality/main.py
@@ -1,8 +1,7 @@
-import TEETHRECOGNITIONWITHMACHINELEARNING.Experiment.Tonality.ReadImages as rI
+import Experiment.Tonality.ReadImages as rI
 import os
 
 if __name__== '__main__':
     PATH_IMAGES =os.path.abspath(os.path.join(os.path.join(os.path.join(os.getcwd(), os.pardir),os.pardir),"DATASET"))
     readImages = rI.loadData()
     imagesArray = readImages.readImages(PATH_IMAGES)
-    print(imagesArray)
