46388e3b1d4a4c529286a41efcd68a075d970732
@Arnold0210 <arnoldjulianherreraquiones@gmail.com>
2019-01-21 11:47:40 -0500

    Sync

---
+++ Experiment/Tonality/PreProcessingData.py
@@ -0,0 +1,7 @@
+import TEETHRECOGNITIONWITHMACHINELEARNING.Experiment.Tonality.ReadImages as ri
+
+class PreProcessingData:
+    def BorderDetection(self,images):
+        pass
+    def ImageSegmentation(self,images):
+        pass
