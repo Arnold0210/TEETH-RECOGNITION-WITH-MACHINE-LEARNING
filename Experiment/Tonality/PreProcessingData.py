57cc121c27b37a49c644eb3161b73d350928d810
@Arnold0210 <arnoldjulianherreraquiones@gmail.com>
2019-01-29 10:19:14 -0500

    C00004
    Se añade de documento de anteproyecto, se crean clases para cargar imágenes y convertirlas a B/N

--- Experiment/Tonality/PreProcessingData.py
+++ Experiment/Tonality/PreProcessingData.py
@@ -1,9 +1,18 @@
 import TEETHRECOGNITIONWITHMACHINELEARNING.Experiment.Tonality.ReadImages as ri
+import cv2 as cv
+
 
 class PreProcessingData:
-    def BorderDetection(self,images):
+    def BlackGreyScale(self, images):
+        image_bn = cv.cvtColor(images, cv.COLOR_BGR2GRAY)
+
+        return image_bn
+
+    def BorderDetection(self, images):
         pass
-    def ImageSegmentation(self,images):
+
+    def ImageSegmentation(self, images):
         pass
-    def ImageBinarization(self,images):
+
+    def ImageBinarization(self, images):
         pass
