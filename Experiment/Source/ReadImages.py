bb3cc1febe172250265e24d7cedadb06df3b75bf
@Arnold0210 <arnoldjulianherreraquiones@gmail.com>
2019-02-10 23:52:57 -0500

    C00011
    Se crean archivos .py para la extraccion de caracteristicas, preprocesamiento, se añade código de creación de carpetas en main.py y PreProcessing.py para poder guardar las imágenes modificadas en cada etapa del proceso.
    
    Además se crea una estructura automática de árbol para almacenar los archivos en cada etapa

--- Experiment/Source/ReadImages.py
+++ Experiment/Source/ReadImages.py
@@ -10,8 +10,11 @@ import os, cv2 as cv
 from tqdm import tqdm
 
 
-class loadData:
-    def readImages(self, PATH):
+class LoadData:
+    PATH = ''
+    def __init__(self,PATH_IMAGES):
+        self.PATH = PATH_IMAGES
+    def read_Images(self, PATH):
         images = []
         amount = len(os.listdir(PATH))
         bar = tqdm(os.listdir(PATH), ncols=amount, unit=' image')
@@ -22,8 +25,7 @@ class loadData:
             bar.set_description("Leyendo archivo %s" % i)
         return images
 
-    def readOneImage(self, PATH):
-        imageString = os.path.join(PATH, "100_0055.JPG")
-        image = cv.imread(imageString)
-        image = cv.resize(image, (600, 400))
+    def read_One_Image(self, PATH):
+        image_string = os.path.join(PATH, "100_0055.JPG")
+        image = cv.imread(image_string)
         return image
